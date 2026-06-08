b521b032a3451c8e71c4a46d4cb196014febb78c
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-08 18:30:42 +0100

    Change the life cycle timeout from 24 to 2 hours

--- lib/utils/Helpers/life_cycle_helper.dart
+++ lib/utils/Helpers/life_cycle_helper.dart
@@ -17,7 +17,7 @@ class _AppLifeCycleHelperState extends State<AppLifeCycleHelper> with WidgetsBin
 
   Timer? _timer;
 
-  final Duration timeOut = const Duration(hours: 24);
+  final Duration timeOut = const Duration(hours: 2);
 
   @override
   void initState() {
