0028aa40e57bd933387253fdef8b3e0271183299
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-09 22:28:02 +0100

    Set the verified variable to false when select

--- lib/views/categories/screens/tv/television_bill_view.dart
+++ lib/views/categories/screens/tv/television_bill_view.dart
@@ -1,4 +1,5 @@
 import 'package:fin_wise/controllers/categoryCtrl/television_ctrl.dart';
+import 'package:fin_wise/controllers/controller_exports.dart';
 import 'package:fin_wise/core/Routes/routes.dart';
 
 import 'package:fin_wise/core/app_colors.dart';
@@ -12,19 +13,26 @@ class TelevisionBillView extends StatelessWidget {
   TelevisionBillView({super.key});
 
   final ctrl = Get.find<TelevisionCtrl>();
+
   Future<void> onRefresh() async {
-    Future.delayed(Duration(seconds: 1), ()async => await ctrl.getCableDiscos());
+    Future.delayed(
+      Duration(seconds: 1),
+      () async => await ctrl.getCableDiscos(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: RefreshIndicator(
         onRefresh: onRefresh,
         child: PageContainer(
           topMargin: 20,
-          topChild: CustomAppBar.header(title: 'Cables & Tv', leftRight: 15, onPressed: () => Get.back()),
+          topChild: CustomAppBar.header(
+            title: 'Cables & Tv',
+            leftRight: 15,
+            onPressed: () => Get.back()
+          ),
           child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
@@ -47,43 +55,43 @@ class TelevisionBillView extends StatelessWidget {
                     ),
                   ),
                 ),
-                const SizedBox(height: 10,),
+                const SizedBox(height: 10),
                 Expanded(
-                  child: Obx((){
-                    if(ctrl.loadDisco.value){
-                      return Center(child: SkeletonLoader.shimmerLines(len: 4),);
-                    }else if(ctrl.availableCable.isEmpty){
+                  child: Obx(() {
+                    if (ctrl.loadDisco.value) {
+                      return Center(child: SkeletonLoader.shimmerLines(len: 4));
+                    } else if (ctrl.availableCable.isEmpty) {
                       return EmptyState(message: ctrl.discoErr.value);
                     }
-                      return ListView.separated(
-                        padding: const EdgeInsets.only(top: 15),
-                        itemBuilder: (context, index) {
-                          final tv = ctrl.availableCable[index];
-                          return ListTile(
-                            onTap: () {
-                              Get.toNamed(Routes.tvSubscription, arguments: tv);
-                            },
-                            title: Text(tv.name, overflow: TextOverflow.ellipsis),
-                            subtitle: Text(
-                              tv.serviceId.toUpperCase(),
-                              style: TextStyle(color: AppColors.primary),
+                    return ListView.separated(
+                      padding: const EdgeInsets.only(top: 15),
+                      itemBuilder: (context, index) {
+                        final tv = ctrl.availableCable[index];
+                        return ListTile(
+                          onTap: () {
+                            ctrl.verified.value = false;
+                            Get.toNamed(Routes.tvSubscription, arguments: tv);
+                          },
+                          title: Text(tv.name, overflow: TextOverflow.ellipsis),
+                          subtitle: Text(
+                            tv.serviceId.toUpperCase(),
+                            style: TextStyle(color: AppColors.primary),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundColor: AppColors.subBlue,
+                            child: AppText(
+                              text: tv.name[0],
+                              textColor: AppColors.bgColor,
+                              textSize: 25,
                             ),
-                            leading: CircleAvatar(
-                              backgroundColor: AppColors.subBlue,
-                              child: AppText(
-                                text: tv.name[0],
-                                textColor: AppColors.bgColor,
-                                textSize: 25,
-                              ),
-                            ),
-                          );
-                        },
-                        separatorBuilder: (_, index) =>
-                            const Divider(color: AppColors.lightGreen),
-                        itemCount: ctrl.availableCable.length,
-                      );
-                    }
-                  ),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (_, index) =>
+                          const Divider(color: AppColors.lightGreen),
+                      itemCount: ctrl.availableCable.length,
+                    );
+                  }),
                 ),
               ],
             ),
