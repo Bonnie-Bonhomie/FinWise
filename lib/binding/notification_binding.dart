a82cb02f35ffbb09e10e8aa75fda2095f30ebda3
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-12 20:21:52 +0100

    Added notification repo to binding

--- lib/binding/notification_binding.dart
+++ lib/binding/notification_binding.dart
@@ -1,4 +1,8 @@
 import 'package:fin_wise/controllers/notificationCtrl/notify_controller.dart';
+import 'package:fin_wise/core/connection/network.dart';
+import 'package:fin_wise/data/dataSource/api_service.dart';
+import 'package:fin_wise/data/dataSource/storage_file.dart';
+import 'package:fin_wise/data/repositories/notification_repo.dart';
 import 'package:get/get.dart';
 
 class NotifyBinding extends Bindings{
@@ -6,6 +10,7 @@ class NotifyBinding extends Bindings{
   @override
   void dependencies() {
     // TODO: implement dependencies
-    Get.lazyPut<NotifyCtrl>(() => NotifyCtrl(), fenix: true);
+    Get.lazyPut(() => NotificationRepo(Get.find<ApiServices>(), Get.find<InternetService>()), fenix: true);
+    Get.lazyPut<NotifyCtrl>(() => NotifyCtrl(Get.find<NotificationRepo>(), Get.find<StorageFile>()), fenix: true);
   }
 }
