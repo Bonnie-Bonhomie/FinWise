fb83a8ac2f36a1d907d82eaba30251ddbf8b857f
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-14 01:44:33 +0100

    Reduce the app image size

--- lib/views/onboards/splash_screen.dart
+++ lib/views/onboards/splash_screen.dart
@@ -56,8 +56,8 @@ class _SplashScreenState extends State<SplashScreen>{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image(image: AssetImage(PrefStoreKeys.appWhiteImage)),
-            SizedBox(height: 10,),
+            Image(image: AssetImage('Assets/logos/white_logo.png', ), width: 120, height: 120,),
+            // SizedBox(height: 10,),
             const AppText(text: 'DatabootNg, all service at glance', textColor: Colors.white, textSize: 12, textWeigh: FontWeight.bold),
           ],
         ),
