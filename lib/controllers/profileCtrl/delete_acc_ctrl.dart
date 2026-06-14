a7cfd3cac219f9fde5f644121403291c8eb7e995
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-14 22:02:34 +0100

    Added delete function

--- lib/controllers/profileCtrl/delete_acc_ctrl.dart
+++ lib/controllers/profileCtrl/delete_acc_ctrl.dart
@@ -1,13 +1,60 @@
-import 'package:fin_wise/core/connection/network.dart';
-import 'package:fin_wise/data/dataSource/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:fin_wise/core/Routes/routes.dart';
+import 'package:fin_wise/core/resources/data_state.dart';
+import 'package:fin_wise/core/resources/storage_keys.dart';
+import 'package:fin_wise/data/dataSource/storage_file.dart';
+import 'package:fin_wise/data/repositories/profileRepo/delete_acc_repo.dart';
+import 'package:fin_wise/utils/Helpers/share_prefer_services.dart';
+import 'package:fin_wise/utils/widgets/custom_snackbar.dart';
+import 'package:get/get.dart';
 
-class DeleteAccRepo{
+class DeleteCtrl extends GetxController{
 
+  final DeleteAccRepo repo;
+  final StorageFile store;
+  final SharedPreferService shareStore;
 
-  final ApiServices services;
-  final InternetInfo info;
+  DeleteCtrl(this.repo, this.store, this.shareStore);
 
-  DeleteAccRepo(this.services, this.info);
+  Future<void> deleteAccount() async {
+    try{
+      final String? token = await store.getToken();
+      print('Token: $token');
+      if (token == null) {
+        CustomSnackbar.showSnackbar(message: 'Something went wrong');
+      } else {
+        final response = await repo.deleteAccount(token);
+        if(response is DataSuccess){
+          if(response.data['status'] == true){
+            await store.deleteToken();
+            await shareStore.deleteValue(PrefStoreKeys.mail);
+            await shareStore.deleteValue(PrefStoreKeys.username);
+            await shareStore.deleteValue(PrefStoreKeys.userId);
+            Get.offAllNamed(Routes.login);
+          }
+
+        }else if(response is DataFailed){
+
+          final err = response.exception;
+
+          if(err is DioException){
+            if(err.type == DioExceptionType.connectionError ||
+                err.type == DioExceptionType.receiveTimeout ||
+                err.type == DioExceptionType.connectionTimeout ){
+              CustomSnackbar.showSnackbar(message: 'Check your internet connection', title: 'No network');
+            }
+            final errData = err.response?.data;
+
+            if(errData != null && errData['message'] != null){
+              CustomSnackbar.showSnackbar(message: errData['message'].toString());
+            }
+          }
+        }
+
+      }}catch(e){
+      CustomSnackbar.showSnackbar(message: 'Something went wrong try again later', title: 'Oops');
+    }
+  } //Logout
 
 
 }
