d4d54a70489123d7cc818b4f4408997cdbc98551
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-14 21:03:14 +0100

    Added delete account from controller

--- lib/views/profiles/screens/settings/delete_account.dart
+++ lib/views/profiles/screens/settings/delete_account.dart
@@ -1,6 +1,7 @@
 
 import 'package:fin_wise/controllers/profileCtrl/main_ctrl.dart';
 import 'package:fin_wise/core/app_colors.dart';
+import 'package:fin_wise/utils/widgets/LoadingFiles/loading_wrapper.dart';
 import 'package:fin_wise/utils/widgets/app_btn.dart';
 import 'package:fin_wise/utils/widgets/custom_app_bar.dart';
 import 'package:fin_wise/utils/widgets/text_widget.dart';
@@ -10,81 +11,86 @@ import 'package:fin_wise/views/view_widgets/view_container.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../../controllers/controller_exports.dart';
+
 class DeleteAccount extends StatelessWidget {
-  const DeleteAccount({super.key});
+  DeleteAccount({super.key});
+
+  final ctrl = Get.find<DeleteCtrl>();
+  final loader = Get.find<LoaderController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageContainer(
-        topMargin: 20,
-        topChild: CustomAppBar.header(
-          title: 'Delete Account',
-          leftRight: 15,
-          onPressed: () => Get.find<ProfileMainControl>().back(),
-        ),
-        child: ListView(
-          padding: const EdgeInsets.fromLTRB(20, 0, 20, 15),
-          children: [
-            const AppText(
-              text: 'Are you sure you want to delete your account',
-              textAlign: TextAlign.center,
-              textWeigh: FontWeight.bold,
-              textSize: 20,
-            ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              margin: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Theme.of(context).cardColor,
-                borderRadius: BorderRadius.circular(15),
+      body: LoaderWrapper(
+        child: PageContainer(
+          topMargin: 20,
+          topChild: CustomAppBar.header(
+            title: 'Delete Account',
+            leftRight: 15,
+            onPressed: () => Get.find<ProfileMainControl>().back(),
+          ),
+          child: ListView(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 15),
+            children: [
+              const AppText(
+                text: 'Are you sure you want to delete your account',
+                textAlign: TextAlign.center,
+                textWeigh: FontWeight.bold,
+                textSize: 20,
               ),
-              child: AppText(text: content, textAlign: TextAlign.start),
-            ),
-            const AppText(
-              text:
-                  'Please enter your password to continue deletion of your account',
-              textAlign: TextAlign.center,
-              textWeigh: FontWeight.bold,
-              textSize: 20,
-            ),
-            const SizedBox(height: 15),
-            SizedBox(
-              height: 40,
-              child: TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Enter password',
-                  suffixIcon: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.visibility_outlined),
-                  ),
+              Container(
+                padding: const EdgeInsets.all(20),
+                margin: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Text(content, textAlign: TextAlign.start),
+              ),
+              const Text(
+                    'Please enter your password to continue deletion of your account',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 15),
+              SizedBox(
+                height: 40,
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter password',
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.visibility_outlined),
+                    ),
 
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 15),
-            AppBtn(
-              onPressed: () {
-                showDelDialog(context);
-              },
-              label: 'Yes, Delete Account',
-            ),
-            const SizedBox(height: 15),
-            ElevatedButton(
-              onPressed: () {
-                Get.find<ProfileMainControl>().back();
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).cardColor,
+              const SizedBox(height: 15),
+              AppBtn(
+                onPressed: () {
+                  showDelDialog(context);
+                },
+                label: 'Yes, Delete Account',
               ),
-              child: const AppText(
-                text: 'Cancel',
-                // textColor: AppColors.darkGreen,
-                textSize: 18,
+              const SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: () {
+                  Get.find<ProfileMainControl>().back();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).cardColor,
+                ),
+                child: const AppText(
+                  text: 'Cancel',
+                  // textColor: AppColors.darkGreen,
+                  textSize: 18,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -95,20 +101,19 @@ class DeleteAccount extends StatelessWidget {
       context: context,
       builder: (context) {
         return AlertDialog(
-          backgroundColor: AppColors.bgColor,
+          // backgroundColor: AppColors.bgColor,
           title: const HeadingText(headingText: 'Delete Account'),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              const AppText(
-                text: 'Are you sure you want to delete account?',
-                textWeigh: FontWeight.bold,
-                textSize: 18,
+              const Text(
+                'Are you sure you want to delete account?',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
                 textAlign: TextAlign.center,
               ),
               const SizedBox(height: 20),
-              const AppText(
-                text:
+              const Text(
+
                     'By deleting your account, you agree that you understand the consequence of this action and that you agree to permanently delete your account and all associated data.',
                 textAlign: TextAlign.center,
               ),
@@ -116,7 +121,12 @@ class DeleteAccount extends StatelessWidget {
           ),
           actions: [
             AppBtn(
-              onPressed: () {},
+              onPressed: () {
+                Get.back();
+                loader.offLoading(() async{
+                  await ctrl.deleteAccount();
+                });
+              },
               label: 'Yes, Delete Account',
               textSize: 15,
             ),
