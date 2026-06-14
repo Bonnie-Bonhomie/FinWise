5bf10b88ee17720f41b9083babeb63a33c695c7c
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-14 21:30:42 +0100

    Added delete function

--- lib/data/repositories/profileRepo/delete_acc_repo.dart
+++ lib/data/repositories/profileRepo/delete_acc_repo.dart
@@ -1,45 +1,34 @@
-// import 'package:dio/dio.dart';
-// import 'package:fin_wise/core/Routes/Api_endpoints/api_endpoints.dart';
-// import 'package:fin_wise/core/connection/network.dart';
-// import 'package:fin_wise/data/dataSource/api_service.dart';
-// import 'package:fin_wise/core/resources/data_state.dart';
-//
-// class ChangePinRepo {
-//   final ApiServices apiServices;
-//   final InternetInfo info;
-//
-//   ChangePinRepo(this.apiServices, this.info);
-//
-//   Future<DataState> changePin({
-//     required String currentPin,
-//     required String newPin,
-//     required String confirmPin,
-//     required String token,
-//   }) async {
-//     try {
-//       if (!await info.connected) {
-//         return DataFailed(
-//           DioException(
-//             requestOptions: RequestOptions(path: ''),
-//             error: 'No internet connection',
-//           ),
-//         );
-//       }
-//
-//       final result = await apiServices.postRequestsWithToken(ApiEndpoints.changePin,  token, {
-//         'current_pin': currentPin,
-//         'new_pin': newPin,
-//         'confirm_pin': confirmPin,
-//       });
-//
-//       return DataSuccess(result.data);
-//     } catch (e) {
-//       return DataFailed(
-//         DioException(
-//           requestOptions: RequestOptions(path: ''),
-//           error: e.toString(),
-//         ),
-//       );
-//     }
-//   }
-// }
+import 'package:dio/dio.dart';
+import 'package:fin_wise/core/Routes/Api_endpoints/api_endpoints.dart';
+import 'package:fin_wise/core/connection/network.dart';
+import 'package:fin_wise/core/resources/data_state.dart';
+import 'package:fin_wise/data/dataSource/api_service.dart';
+
+class DeleteAccRepo{
+
+
+  final ApiServices services;
+  final InternetInfo info;
+
+  DeleteAccRepo(this.services, this.info);
+
+  Future<DataState> deleteAccount(String token) async {
+
+    try {
+
+      if (!await info.connected) {
+        return DataFailed(
+          DioException(
+            requestOptions: RequestOptions(path: ''),
+            error: 'No internet connection',
+          ),
+        );
+      }
+      final result = await services.deleteRequestWIthToken(ApiEndpoints.deleteAccount, token);
+      return DataSuccess(result.data);
+    }on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
+
+}
