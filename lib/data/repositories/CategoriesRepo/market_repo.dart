21c0cf4a2f824f6f54482bd85bfa0db540bbf695
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-18 11:26:50 +0100

    Added new notification sign

---
+++ lib/data/repositories/CategoriesRepo/market_repo.dart
@@ -0,0 +1,76 @@
+import 'package:dio/dio.dart';
+import 'package:fin_wise/core/Routes/Api_endpoints/api_endpoints.dart';
+import 'package:fin_wise/core/connection/network.dart';
+import 'package:fin_wise/core/resources/data_state.dart';
+import 'package:fin_wise/data/dataSource/api_service.dart';
+
+class MarketRepo{
+
+  final ApiServices services;
+  final InternetInfo info;
+
+  MarketRepo(this.services, this.info);
+
+  Future<DataState> getProducts(String token) async {
+    try {
+      if (!await info.connected) {
+        return DataFailed(
+          DioException(
+            requestOptions: RequestOptions(path: ''),
+            type: DioExceptionType.connectionError,
+            error: 'No internet connection',
+          ),
+        );
+      } else {
+        final result = await services.getRequestWIthToken(ApiEndpoints.fishMarket, token);
+        return DataSuccess(result.data);
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
+
+  Future<DataState> getSingle(String token, int id) async {
+    try {
+      if (!await info.connected) {
+        return DataFailed(
+          DioException(
+            requestOptions: RequestOptions(path: ''),
+            type: DioExceptionType.connectionError,
+            error: 'No internet connection',
+          ),
+        );
+      } else {
+        final result = await services.getRequestWIthToken('${ApiEndpoints.singleFish}/$id', token);
+        return DataSuccess(result.data);
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
+
+  Future<DataState> buyProduct({required String token, required String productId, required String deliveryAdd, required String phoneNo, required String transPin, required String quantity}) async {
+    try {
+      if (!await info.connected) {
+        return DataFailed(
+          DioException(
+            requestOptions: RequestOptions(path: ''),
+            type: DioExceptionType.connectionError,
+            error: 'No internet connection',
+          ),
+        );
+      } else {
+        final result = await services.postRequestsWithToken(ApiEndpoints.airtimeNetwork, token, {
+          'product_id': productId,
+          'delivery_address': deliveryAdd,
+          'quantity': quantity,
+          'phone_number': phoneNo,
+          'transaction_pin': transPin
+        });
+        return DataSuccess(result.data);
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
+}
