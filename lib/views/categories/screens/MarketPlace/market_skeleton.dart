c62a533498491dd80210e10b4230c634a321029d
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-19 01:19:45 +0100

    Market loading skeleton

---
+++ lib/views/categories/screens/MarketPlace/market_skeleton.dart
@@ -0,0 +1,35 @@
+import 'package:fin_wise/utils/utils_export.dart';
+import 'package:flutter/material.dart';
+
+class MarketSkeleton extends StatelessWidget {
+  const MarketSkeleton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context, index ){
+      return SkeletonLoader.loadingSkeleton(child:
+      Column(
+        children: [
+          Container(
+            height: 150,
+            width: 300,
+           margin: const EdgeInsets.all(6),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade400),
+          ),
+
+          Container(
+            height: 15,
+            width: 200,
+            margin: const EdgeInsets.all(6),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade400),
+          ),Container(
+            height: 15,
+            width: 300,
+            margin: const EdgeInsets.all(6),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade400),
+          ),
+        ],
+      ));
+    }, itemCount: 5);
+  }
+}
