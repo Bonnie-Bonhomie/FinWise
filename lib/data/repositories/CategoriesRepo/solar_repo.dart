ce8d2622ad86706a5892f1c835f79cba04a37588
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-19 01:20:44 +0100

    Solar Repository

---
+++ lib/data/repositories/CategoriesRepo/solar_repo.dart
@@ -0,0 +1,12 @@
+import 'package:fin_wise/core/connection/network.dart';
+import 'package:fin_wise/data/dataSource/api_service.dart';
+
+class SolarRepo {
+
+  final ApiServices services;
+  final InternetInfo info;
+
+  SolarRepo(this.services, this.info);
+
+
+}
