a35cf7013240f8d69c09facb3f1e19407bf25a83
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-20 20:21:41 +0100

    Added faqs function

--- lib/controllers/profileCtrl/help_center_ctrl.dart
+++ lib/controllers/profileCtrl/help_center_ctrl.dart
@@ -1,14 +1,82 @@
+import 'package:dio/dio.dart';
+import 'package:fin_wise/core/resources/data_state.dart';
+import 'package:fin_wise/data/dataSource/storage_file.dart';
 import 'package:fin_wise/data/models/chat_model.dart';
 import 'package:fin_wise/data/models/faqs_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
+import '../../data/repositories/repo_exports.dart';
+import '../../utils/utils_export.dart';
+
 class HelpControl extends GetxController {
+
+  final HelpCenterRepo repo;
+  final StorageFile store;
+
+  HelpControl(this.repo, this.store);
+
   final ScrollController scroll = ScrollController();
   var faqSections = ['General', 'Account', 'Service'];
 
   var faqGeneralQ = [].obs;
   var faqIndex = 0.obs;
+  var loading = false.obs;
+
+  Future<void> getFaqs() async {
+
+    try {
+      loading.value = true;
+      final token = await store.getToken();
+
+      if(token == null){
+        CustomSnackbar.showSnackbar(message: 'Unauthenticated');
+        return;
+      }
+      final response = await repo.getFaqs(token);
+
+      print(response.data);
+      if(response is DataSuccess ){
+
+        print(response.data);
+
+      }
+      else if (response is DataFailed) {
+        final err = response.exception;
+
+        if (err is DioException) {
+          //  Network issues
+          if (err.type == DioExceptionType.connectionError ||
+              err.type == DioExceptionType.receiveTimeout ||
+              err.type == DioExceptionType.connectionTimeout) {
+            CustomSnackbar.showSnackbar(title: 'No internet connection', message: 'Check your internet connection');
+            return;
+          }
+
+          //  Server error
+          final errData = err.response?.data;
+
+          if (errData != null && errData['message'] != null) {
+            CustomSnackbar.showSnackbar(message: errData['message']);
+          } else {
+            CustomSnackbar.showSnackbar(
+              message: 'Server error, try again later',
+            );
+          }
+        } else {
+          CustomSnackbar.showSnackbar(message: 'Unknown error occurred');
+        }
+      }
+    }catch(e){
+      print(e);
+      CustomSnackbar.showSnackbar(message: 'Unknown error occurred');
+    }
+    finally{
+      loading.value = false;
+    }
+  }
+
+
 
   var chats = [
     ChatModel(
