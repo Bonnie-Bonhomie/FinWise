41a2d36eea3dfd6f8a68b571c5579af9f60505c5
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-20 12:55:57 +0100

    Modified the change password function

--- lib/controllers/profileCtrl/change_pwd_ctrl.dart
+++ lib/controllers/profileCtrl/change_pwd_ctrl.dart
@@ -1,8 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:fin_wise/core/Routes/routes.dart';
 import 'package:fin_wise/core/app_colors.dart';
 import 'package:fin_wise/data/dataSource/storage_file.dart';
 import 'package:fin_wise/data/repositories/profileRepo/change_pwd_repo.dart';
 import 'package:fin_wise/core/resources/data_state.dart';
+import 'package:fin_wise/utils/utils_export.dart';
 import 'package:get/get.dart';
+
+import '../controller_exports.dart';
 
 class ChangePwdControl extends GetxController {
 
@@ -15,6 +20,7 @@ class ChangePwdControl extends GetxController {
   var pwdObscure = true.obs;
   var confirmPwdObscure = true.obs;
   var loading = false.obs;
+  var successMessage = ''.obs;
   var error = ''.obs;
 
 
@@ -27,24 +33,53 @@ class ChangePwdControl extends GetxController {
     required String newPwd,
     required String confirmPwd
   }) async {
-    loading.value = true;
-    final token = await storage.getToken();
 
-    if(token == null){
-      Get.snackbar('Error', 'User not authenticated');
-      return;
-    }
     try {
-      final response = await repo.submitPwd(
+      loading.value = true;
+      final token = await storage.getToken();
+
+      if(token == null){
+        CustomSnackbar.showSnackbar(message: 'Unauthenticated');
+        return;
+      }
+      final response = await repo.changePwd(
         token: token,
           currentPwd: currentPwd, newPwd: newPwd, confirmPwd: confirmPwd);
 
-      if(DataState is DataSuccess && response.data['status'] == 'Success'){
-        error.value = '';
+      if(response is DataSuccess ){
+        if(response.data['status'] == true){
+          // final data = response.data['data'];
+          successMessage =
+              response.data['message'] ?? 'Password has been changed successfully';
+          Get.find<ProfileMainControl>().back();
+          Get.toNamed(Routes.successful, arguments: successMessage);
+        }
       }
-      else{
-        error.value = 'Unable to change Password';
-        Get.snackbar('Oops', error.value, backgroundColor: AppColors.bgColor);
+      else if (response is DataFailed) {
+        final err = response.exception;
+
+        if (err is DioException) {
+          //  Network issues
+          if (err.type == DioExceptionType.connectionError ||
+              err.type == DioExceptionType.receiveTimeout ||
+              err.type == DioExceptionType.connectionTimeout) {
+            CustomSnackbar.showSnackbar(title: 'No internet connection', message: 'Check your internet connection');
+            return;
+          }
+
+          //  Server error
+          final errData = err.response?.data;
+
+          if (errData != null && errData['message'] != null) {
+            CustomSnackbar.showSnackbar(message: errData['message']);
+          } else {
+            CustomSnackbar.showSnackbar(
+              message: 'Server error, try again later',
+            );
+          }
+        } else {
+          CustomSnackbar.showSnackbar(message: 'Unknown error occurred');
+        }
       }
     }catch(e){
       print(e);
