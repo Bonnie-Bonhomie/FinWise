691a36ccf0587f1d3af9058d07c97a1ad0581abc
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-20 18:06:53 +0100

    Added Center repository

---
+++ lib/data/repositories/profileRepo/help_center_repo.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:fin_wise/core/Routes/Api_endpoints/api_endpoints.dart';
+import 'package:fin_wise/core/connection/network.dart';
+import 'package:fin_wise/data/dataSource/api_service.dart';
+import 'package:fin_wise/core/resources/data_state.dart';
+
+class HelpCenterRepo {
+  final ApiServices apiServices;
+  final InternetInfo internet;
+
+  HelpCenterRepo(this.apiServices, this.internet);
+
+  Future<DataState> getFaqs(String token) async {
+
+    try {
+
+      if (!await internet.connected) {
+        return DataFailed(
+          DioException(
+            requestOptions: RequestOptions(path: ''),
+            type: DioExceptionType.connectionError,
+            error: 'No internet connection',
+          ),
+        );
+      }
+      final result = await apiServices.getRequestWIthToken(ApiEndpoints.faqs, token);
+      return DataSuccess(result.data);
+    }on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
+}
