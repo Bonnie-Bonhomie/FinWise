0c124acd4bda419d14de97cdecf3a2304e819969
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-22 18:00:06 +0100

    Modified the model to api status

--- lib/data/models/faqs_model.dart
+++ lib/data/models/faqs_model.dart
@@ -1,10 +1,31 @@
-
-class FaqsModel{
-
-  String question;
-  String answer;
+class FaqsModel {
+  String id;
+  String title;
+  String description;
+  String createdAt;
+  String slug;
+  String type;
+  String status;
 
   FaqsModel({
-    required this.question, required this.answer
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.createdAt,
+    required this.slug,
+    required this.type,
+    required this.status,
   });
+
+  factory FaqsModel.fromJson(Map<String, dynamic> json) {
+    return FaqsModel(
+      id: json['id'].toString(),
+      title: json['title'].toString(),
+      description: json['description'].toString(),
+      createdAt: json['created_at'].toString(),
+      slug: json['slug'].toString(),
+      type: json['type'].toString(),
+      status: json['status'].toString(),
+    );
+  }
 }
