a0c989049aa1acf6181a7d7312a92993784a849c
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-06-22 18:21:50 +0100

    Modified frame work function

--- lib/main.dart
+++ lib/main.dart
@@ -8,34 +8,26 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await FirebaseMsgService().initFCM();
 
-
+  listenToTokenRefresh();
 
   DependencyInjection.init();
 
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        statusBarIconBrightness: Brightness.dark,
-        statusBarBrightness: Brightness.light,
-        statusBarColor: Colors.transparent,
-      )
+    SystemUiOverlayStyle(
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.light,
+      statusBarColor: Colors.transparent,
+    ),
   );
-    print('App Started');
 
-  runApp(
-    DismissKeyboard(child: AppLifeCycleHelper(child: const MyApp()))
-  );
+  runApp(DismissKeyboard(child: AppLifeCycleHelper(child: const MyApp())));
 }
